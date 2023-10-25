041c46d0b18143e745b0e2e73c4e034feadcd899
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2023-10-25 09:10:03 +0300

    chore: refactor mime types

---
+++ src/utils/mimeTypes.ts
@@ -0,0 +1,12 @@
+export const defaultPhotoMimeTypes = [
+  'image/jpeg',
+  'image/jp2',
+  'image/jpx',
+  'image/jpm',
+  'image/jxr',
+  'image/jxl',
+  'image/png',
+  'image/tiff',
+  'image/webp',
+  'image/avif',
+];

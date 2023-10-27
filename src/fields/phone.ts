5b4b8a8e481004669fd6ef312c8a5994927ee6a1
Bohdan Kucherivayi <bohdan.kucheriavyi@zapal.tech>
2023-10-27 08:53:32 +0300

    fix: remove phone spec. field

--- src/fields/phone.ts
+++ src/fields/phone.ts
@@ -1,9 +1,8 @@
 import { TextField } from 'payload/types';
 
-import { phoneField as pluginPhoneField } from 'payload-plugin-phone-field';
-
-export const phoneField = (options?: Omit<TextField, 'type'>, phoneOptions?: any) =>
-  pluginPhoneField(
-    { name: 'phone', label: { en: 'Phone', ua: 'Телефон' }, ...options },
-    { smartCaret: true, withCountryCallingCode: true, international: true, countrySelectComponent: () => null },
-  );
+export const phoneField = (options?: Omit<TextField, 'type'>): TextField => ({
+  type: 'text',
+  name: 'phone',
+  label: { en: 'Phone', ua: 'Телефон' },
+  ...options,
+});
